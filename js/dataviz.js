1c52da206f7a14ee678db83cbabf27cf5866ea1a
numeroteca <pablo@basurama.org>
2013-08-13 18:38:53 +0200

    Adds circle visualization to the graph. Adds color legend.

--- js/dataviz.js
+++ js/dataviz.js
@@ -72,10 +72,15 @@ svg.append("image")
 var donationslines = svg.append('g').attr('class','donationslines');
 var donationslinesnotime = svg.append('g').attr('class','donationslinesnotime');
 
-//set elctions lines
+//set elections lines
 var elections = svg.append('g').attr('class','elections electionschart');
 
-/*
+//sets saldos dots
+var saldosdots = svg.append('g').attr('class','saldosdots');
+
+//sets entradas circulos
+var entradascirculos = svg.append('g').attr('id','circulos');
+
 //Line Chart
 var lineFunction = d3.svg.line()
 	.interpolate("linear")
@@ -89,7 +94,7 @@ var lineFunction2 = d3.svg.line()
 //xScale(parseDate('07-06-2007')); 
 	.x(function(d,i) { return i*barwidth; })
 	.y(function(d) { return yScale(d.SaldoAnotado);}); 
-*/
+
 d3.tsv("data/viplist.tsv", function(error, data) {//reads the data.tsv file
 	
 	//Creates Legend for notime graph
@@ -117,6 +122,7 @@ d3.tsv("data/viplist.tsv", function(error, data) {//reads the data.tsv file
 				}
 	  		});
 
+
 	//Creates Legend for time graph
 	var legend = d3.select("#legend").attr("class", "legend");
 	legend.append("h5").style("font-weight","bold").text("Selecciona una persona"); //legend title
@@ -127,34 +133,85 @@ d3.tsv("data/viplist.tsv", function(error, data) {//reads the data.tsv file
 		.text(function(d) { return d.people; })
 		.on('click',function(d) { //when click on name
 			var personflat = d.people.replace(/\s+/g, '').replace('.', ''); //removes spaces and . from person name
+			if (d3.select(this).attr('class')==='inactive btn btn-default btn-xs donante' 
+				|| d3.select(this).attr('class')==='inactive btn btn-default btn-xs beneficiario' 		
+				|| d3.select(this).attr('class')==='inactive btn btn-default btn-xs donabenef'){
+				//first time
+				d3.selectAll('svg .bar').attr("opacity",1)
+				d3.selectAll('svg .bar').transition().duration(500).attr("opacity",.05); //dims all bars
+				d3.selectAll('svg .bar.'+personflat).transition().duration(2500).attr("class","bar highlighted "+personflat); //adds class "highlighted" and person related class to the bar
+				d3.select(this).transition().duration(0).attr("class","btn-warning btn btn-default btn-xs"); //adds class .active to button
+			//second time
+			} else if (d3.select(this).attr('class')==='btn-warning btn btn-default btn-xs'){
+				d3.select(this).attr("class",function(d) { return "inactive btn btn-default btn-xs " + d.tipo;}); //removes .active class
+				d3.selectAll("svg .highlighted."+ personflat).attr("class","bar "+ personflat);
+				d3.selectAll('svg .bar').transition().duration(500).attr("opacity",.15);
+			}
+		});
+
+	//Creates Legend for time graph Circles
+	var legend = d3.select("#legendcirculos").attr("class", "legend");
+	legend.append("h5").style("font-weight","bold").text("Selecciona una persona"); //legend title
+	legend.selectAll('div')
+		.data(data)
+		.enter().append("div")
+		.attr("class", function(d) { return "inactive btn btn-default btn-xs " + d.tipo;})
+		.text(function(d) { return d.people; })
+		.on('click',function(d) { //when click on name
+			var personflat = d.people.replace(/\s+/g, '').replace('.', ''); //removes spaces and . from person name
 				if (d3.select(this).attr('class')==='inactive btn btn-default btn-xs donante' || d3.select(this).attr('class')==='inactive btn btn-default btn-xs beneficiario' || d3.select(this).attr('class')==='inactive btn btn-default btn-xs donabenef'){
 				//first time
-					d3.selectAll('svg .bar').attr("opacity",1)
-					d3.selectAll('svg .bar').transition().duration(500).attr("opacity",.05); //dims all bars 
-					    	d3.selectAll('svg .bar.'+personflat).transition().duration(2500).attr("class","bar highlighted "+personflat); //adds class "highlighted" and person related class to the bar
+					d3.selectAll('svg .circulos').attr("opacity",1)
+					d3.selectAll('svg .circulos').transition().duration(500).attr("opacity",.1); //dims all bars 
+					d3.selectAll('svg .circulos.'+personflat).transition().duration(2500).attr("class","circulos highlighted "+personflat); //adds class "highlighted" and person related class to the bar
 					d3.select(this).transition().duration(0).attr("class","btn-warning btn btn-default btn-xs"); //adds class .active to button
 
 				//second time
 				} else if (d3.select(this).attr('class')==='btn-warning btn btn-default btn-xs'){
-				  d3.select(this).attr("class",function(d) { return "inactive btn btn-default btn-xs " + d.tipo;}); //removes .active class
-					d3.selectAll("svg .highlighted."+  personflat).attr("class","bar "+  personflat);
-					d3.selectAll('svg .bar').transition().duration(500).attr("opacity",.15);
+					d3.select(this).attr("class",function(d) { return "inactive btn btn-default btn-xs " + d.tipo;}); //removes .active class
+					d3.selectAll("svg .highlighted."+  personflat).attr("class","circulos "+  personflat);
+					d3.selectAll('svg .circulos').transition().duration(500).attr("opacity",.9);
 				}
 	  		});
 });
 
 //Switch between graphs with and without time scales
 d3.selectAll(".btn-group .btn").on('click', function() {//when click //
-	if (d3.select(this).attr('id')==='contiempo'){/// with time
+	if (d3.select(this).attr('id')==='contiempo'){ // with time
 		if (d3.select(this).attr('class')==='btn btn-small btn-danger'){ // si activo
 			//no hacer nada
 		} else {						//si estaba desactivado
 			d3.select(this).attr("class","btn btn-small btn-danger"); //adds class .active to button
 			d3.select("#sintiempo").attr("class","btn btn-small");
+			d3.select("#circulos").attr("class","btn btn-small");
 			d3.selectAll("svg .bar").style("display","block");	
 			d3.selectAll("svg .x.axis").style("display","block");
+			d3.selectAll("svg .y.axis").style("display","block");
+			d3.selectAll("svg .donaciones").style("display","block");
 			d3.selectAll("svg .donationslines").style("display","block");
 			d3.select("#legend").style("display","block");
+			d3.selectAll("svg .electionschart").style("display","block");
+			d3.selectAll("svg .circulos").style("display","none");
+			d3.select("#legendnotime").style("display","none");
+			d3.select("#legendcirculos").style("display","none");
+			d3.selectAll("svg .barnotime").style("display","none");
+			d3.selectAll("svg .donationslinesnotime").style("display","none");
+		}
+	} else if (d3.select(this).attr('id')==='circulos'){ //with time + circles
+		if (d3.select(this).attr('class')==='btn btn-small btn-danger'){ // si activo
+			//no hacer nada
+		} else {						//si estaba desactivado
+			d3.select(this).attr("class","btn btn-small btn-danger"); //adds class .active to button
+			d3.select("#sintiempo").attr("class","btn btn-small");
+			d3.select("#contiempo").attr("class","btn btn-small");
+			d3.selectAll("svg .bar").style("display","none");	
+			d3.selectAll("svg .donaciones").style("display","none");
+			d3.selectAll("svg .circulos").style("display","block");
+			d3.selectAll("svg .x.axis").style("display","block");
+			d3.selectAll("svg .y.axis").style("display","none");
+			d3.selectAll("svg .donationslines").style("display","block");
+			d3.select("#legendcirculos").style("display","block");
+			d3.select("#legend").style("display","none");
 			d3.selectAll("svg .electionschart").style("display","block");
 			d3.select("#legendnotime").style("display","none");
 			d3.selectAll("svg .barnotime").style("display","none");
@@ -166,15 +223,20 @@ d3.selectAll(".btn-group .btn").on('click', function() {//when click //
 		} else {
 			d3.select(this).attr("class","btn btn-small btn-danger"); //adds class .active to button
 			d3.select("#contiempo").attr("class","btn btn-small");
+			d3.select("#circulos").attr("class","btn btn-small");
 			d3.selectAll("svg .barnotime").style("display","block");
 			d3.select("#legendnotime").style("display","block");
 			d3.selectAll("svg .donationslinesnotime").style("display","block");
+			d3.selectAll("svg .y.axis").style("display","block");
+			d3.selectAll("svg .donaciones").style("display","block");
 			d3.select("#legend").style("display","none");
 			d3.selectAll("svg .bar").style("display","none");
+			d3.selectAll("svg .circulos").style("display","none");
 			d3.selectAll("svg .xaxis").style("display","none");
 			d3.selectAll("svg .x.axis").style("display","none");
 			d3.selectAll("svg .electionschart").style("display","none");
 			d3.selectAll("svg .donationslines").style("display","none");
+			d3.select("#legendcirculos").style("display","none");
 		}
 	}
 });
@@ -372,10 +434,10 @@ d3.tsv("data/data.tsv", type, function(error, data) {//reads the data.tsv file
 			return d.persona.replace(/\s+/g, '').replace(/\./g, '') +" bar"; //sets the name of the person without spaces as class for the bar
 		}) 
 	//The tooltips
-      .attr("x", function(d) { return xScale(d.date); })
-      .attr("width", barwidth+1)
-      .attr("y", function(d) { return yScale(Math.max(0, d.entradas)); })
-      .attr("height", function(d) { return Math.abs(yScale(d.entradas) - yScale(0)); })
+	.attr("x", function(d) { return xScale(d.date); })
+	.attr("width", barwidth+1)
+	.attr("y", function(d) { return yScale(Math.max(0, d.entradas)); })
+	.attr("height", function(d) { return Math.abs(yScale(d.entradas) - yScale(0)); })
 		.on("mouseover", function(d) {      
 		    div.transition()        
 			.duration(200)      
@@ -534,6 +596,50 @@ d3.tsv("data/data.tsv", type, function(error, data) {//reads the data.tsv file
 
 	svg.append("path")
 		.attr("d", function(d) { return lineFunction2(data);}).attr("class","saldoanotado");*/
+	
+	//saldos dots
+	/*svg.selectAll("circle")
+	.data(data)
+	.enter().append("circle")
+	.attr("cx", function(d) { return xScale(d.date); })
+	//.attr("cx", function(d,i) { return i*barwidth;})
+	.attr("cy",function(d) { return yScale(d.SaldoAnotado);})
+	.attr("r", 1.5);*/
+
+	entradascirculos.selectAll("circle")
+	.data(data)
+	.enter().append("circle")
+	.attr("fill", function(d) { return d.entradas < 0 ? "#C00000" : "#0055D4"; })
+	.attr("cx", function(d) { return xScale(d.date); })
+	//.attr("cx", function(d,i) { return i*barwidth;})
+	.attr("cy",function(d) { return Math.random() * 350 + 20;})
+	.attr("r", function(d) { return Math.abs(d.entradas)/10000; })
+	.attr("opacity", .9)
+	.attr("class", 
+		function(d) { //TODO iterate through array
+			return d.persona.replace(/\s+/g, '').replace(/\./g, '')+" circulos"; //sets the name of the person without spaces as class for the bar
+		}) 
+	.on("mouseover", function(d) {      
+		    div.transition()        
+			.duration(200)      
+			.style("opacity", .9);      
+		    div.html(d.fecha + "<br/><strong/>"  + d.persona + "</strong/><br/>"  + formatComma(d.entradas) + "€ <br/>'"  + d.descripcion + "'" )  
+			.style("left", (d3.event.pageX) + "px")     
+			.style("top", (d3.event.pageY - 0) + "px");    
+		    })                  
+		.on("mouseout", function(d) {       
+		    div.transition()        
+			.duration(500)      
+			.style("opacity", 0);   
+		});
+	/*
+	svg.selectAll("circle")
+	.data(data)
+	.enter().append("circle")
+	.attr("cx", function(d) { return xScale(d.date); })
+	   .attr("cy",function(d) { return yScale(d.SaldoCalculado);})
+	   .attr("r", 1);*/
+
 
 	//The Bars with no time scale
 	svg.selectAll(".barnotime")
